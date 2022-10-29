848342f437227ad24f5718239c955d70e591f203
michaelmlangitan <michaelmlangitan@gmail.com>
2022-10-30 04:05:38 +0700

    add multi chords tag on stripper

--- src/Stripper.ts
+++ src/Stripper.ts
@@ -1,36 +1,18 @@
-import {SONG_PARAGRAPH, SONG_NEW_LINE, SongData, SongDataParagraph} from './functionality'
-
 const PATTERN_CHORD_ROOT = '([cdefgab])'
 const PATTERN_CHORD_SYMBOL = '(#|b)?'
 const PATTERN_CHORD_TYPE = '(m|5|7|maj7|m7|sus4|add9|sus2|7sus4|7#9|9)?'
 const PATTERN_CHORD_SLASH = '(?:\\/([cdefgab])(#|b)?)?'
 const PATTERN_CHORD = '\\s*' + PATTERN_CHORD_ROOT + PATTERN_CHORD_SYMBOL + PATTERN_CHORD_TYPE + PATTERN_CHORD_SLASH + '\\s*'
+const CHORD_TAG = `\\[${PATTERN_CHORD}\\]`
+const MULTI_CHORDS_TAG = `\\[${PATTERN_CHORD}(?:\\-${PATTERN_CHORD})*\\]`
+const ELEMENT_NAME_TAG = '\\{\\s*[\\w\\d]+\\s*\\}'
+const REGEX = `${CHORD_TAG}|${ELEMENT_NAME_TAG}|${MULTI_CHORDS_TAG}`
+const regex = new RegExp(REGEX, 'gi')
 
 export default class Stripper {
     stripScl(str: string, ignoreNewLine?: boolean): string {
-        const pattern = `\\[${PATTERN_CHORD}\\]|\\{\\s*[\\w\\d]+\\s*\\}`
-        const lyrics = str.replace(new RegExp(pattern, 'gi'), '')
+        const lyrics = (typeof str === "string" ? str : '').replace(regex, '')
 
         return ignoreNewLine ? lyrics.replace(/  +/g, ' ') : lyrics.replace(/\s\s+/g, ' ').trim()
-    }
-
-    stripSong(songData: SongData[], ignoreNewLine?: boolean): string {
-        let lyrics = ''
-
-        for (let song of songData) {
-            if (song.type === SONG_PARAGRAPH && Array.isArray(song.contents)) {
-                for (let content of song.contents) {
-                    if (content.lyric)
-                        lyrics += content.lyric
-                }
-            } else if (song.type === SONG_NEW_LINE) {
-                lyrics += '\n'
-            }
-        }
-
-        return ignoreNewLine ? lyrics.replace(/  +/g, ' ') : lyrics.replace(/\s\s+/g, ' ').trim()
-    }
-
-    private songParagraph(contents: SongDataParagraph) {
     }
 }
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,33 +1,14 @@
-// import Manager from "./Manager";
-//
-// type ChordMap = {
-//     base: {
-//         sharp: string[];
-//         flat: string[];
-//     };
-//     symbols: {
-//         sharp: string;
-//         flat: string;
-//     };
-//     types: {
-//         name: string;
-//         alias: string;
-//     }[];
-// };
-//
-// type Options = {
-//     chordSymbol?: '#' | 'b';
-//     transposeStep?: number;
-// };
-//
-// declare const endpoint: {
-//     newManager(options?: Options): Manager;
-//     version: string;
-//     chordMap: ChordMap;
-//     FLAT_SYMBOL: string;
-//     SHARP_SYMBOL: string;
-//     SONG_ELEMENT_NAME: number;
-//     SONG_PARAGRAPH: number;
-//     SONG_NEW_LINE: number;
-// };
-// export default endpoint;
+import Manager from "../src/Manager";
+import { Options, ChordMap } from "../src/functionality";
+
+declare const endpoint: {
+    newManager(options?: Options): Manager;
+    version: string;
+    chordMap: ChordMap;
+    FLAT_SYMBOL: string;
+    SHARP_SYMBOL: string;
+    SONG_ELEMENT_NAME: number;
+    SONG_PARAGRAPH: number;
+    SONG_NEW_LINE: number;
+};
+export default endpoint;
